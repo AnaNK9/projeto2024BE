18e682dd1f50ffff07584d8f0a6dd53985a3060a
AnaNK9 <128160042+AnaNK9@users.noreply.github.com>
2024-03-21 12:25:14 -0300

    Add files via upload

---
+++ Aula3/funcoes.js
@@ -0,0 +1,22 @@
+//let x = "";
+//console.log(x);
+//x = "1"
+
+//1) declara a função
+
+function x(texto){
+    console.log(texto)
+}
+
+//2) executa a função
+
+// imprimeTexto("oi");
+// imprimeTexto("outro texto");
+
+// três formas de escreer funções
+
+function soma(){
+    return 2 + 2;
+}
+
+console.log(soma())
---
+++ Aula3/parametros.js
@@ -0,0 +1,37 @@
+// function soma(numero1, numero2){
+//     return numero1 + numero2;
+// }
+
+// console.log(soma(2,2))
+// console.log(soma(245,20))
+// console.log(soma(-500, 60))
+
+// function nomeIdade(nome, idade){
+//     return `seu nome é ${nome} e minha idade é ${idade}`;
+// }
+
+// function soma(numero1, numero2){
+//     return numero1 + numero2;
+// }
+
+// function multiplica(numero1 = 1, numero2 = 1){
+//     return numero1 * numero2;
+// }
+
+// console.log(multiplica(soma(4, 5)))
+
+
+// boolean
+// true -> verdadeiro
+// false -> falso
+
+const primeiroNumero = 5;
+const segundoNumero = 5;
+let cadastroAtivado = true;
+
+//console.log(primeiroNumero === segundoNumero);
+
+const texto1 = "Alura";
+const texto2 = "alura";
+
+console.log(texto1 === texto2)
