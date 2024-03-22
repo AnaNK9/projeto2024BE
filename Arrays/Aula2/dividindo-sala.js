59b0480e6bd7dfe982b2c0275dbe3b78c8a173b1
AnaNK9 <128160042+AnaNK9@users.noreply.github.com>
2024-03-22 09:53:37 -0300

    Add files via upload

---
+++ Arrays/Aula2/dividindo-sala.js
@@ -0,0 +1,28 @@
+const alunos = [
+    "Ana",
+    "João",
+    "Juliana",
+    "Livia",
+    "Caio",
+    "Lara",
+    "Marjorie",
+    "Guilherme",
+    "Aline",
+    "Fabiana",
+    "Andre",
+    "Carlos",
+    "Paulo",
+    "Bia",
+    "Vivian",
+    "Isabela",
+    "Vinícius",
+    "Renan",
+    "Renata",
+    "Daisy",
+];
+
+const sala1 = alunos.slice(0, 10);
+const sala2 = alunos.slice(10);
+
+console.log(sala1);
+console.log(sala2);
---
+++ Arrays/arrays.js
@@ -0,0 +1,16 @@
+// const notas = [10, 6.5, 8, 7.5];
+// const media = (notas[0] + nota2 + nota3 + nota4) / 4;
+// console.log(media);
+
+
+// const notas = [10, 6, 8];
+// notas.push(7);
+// console.log(notas);
+// const media = (notas[0] + notas[1] + notas[2]) / notas.length;
+// console.log(media);
+
+
+const notas = [10, 6, 8];
+notas.push(7);
+const media = (notas[0] + notas[1] + notas[2] + notas[3]) / notas.length;
+console.log(media);
